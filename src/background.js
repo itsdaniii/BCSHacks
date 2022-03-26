b3fed0cefef00028fcfe33f68c51922373328692
itsdaniii <limdaniellee@gmail.com>
2022-03-26 08:58:21 -0700

    capitalize

--- src/background.js
+++ src/background.js
@@ -3,8 +3,8 @@ async function getCurrentTab() {
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
 };
-  
+
 chrome.tabs.onActivated.addListener(async () => {
     const currentTab = await getCurrentTab();
-    console.log(`tab changed to ${currentTab.url}`);
+    console.log(`Tab changed to ${currentTab.url}`);
 });
